3c12fef5ae98dab9209bf0885ecb03aadfd8c1b2
Anhern Sarsalor <AnhernSarsalor@proton.me>
2025-08-12 21:12:26 +0900

    we have a logo

--- src/lib/components/AnimestrLogo.svelte
+++ src/lib/components/AnimestrLogo.svelte
@@ -4,9 +4,20 @@
 
 {#snippet content()}
 	{#if inline}
-		<span class="text-secondary">Anime</span><span class="text-primary">str</span>
+		<span class="inline-flex h-[0] items-center" style="line-height: 1;">
+			<img
+				src="/logo-without-text.webp"
+				alt="animestr logo"
+				class="w-8 select-none"
+				style="transform: translateY(30%); "
+			/>
+			<span style="transform: translateY(50%);">
+				<span class="text-secondary">Anime</span><span class="text-primary">str</span>
+			</span>
+		</span>
 	{:else}
-		<h1 class="text-2xl font-bold">
+		<h1 class="join items-center text-2xl font-bold">
+			<img src="/logo-without-text.webp" alt="animestr logo" class="w-12 select-none" />
 			<span class="text-secondary">Anime</span><span class="text-primary">str</span>
 			<span class="badge badge-accent badge-sm ml-2">Alpha</span>
 		</h1>
